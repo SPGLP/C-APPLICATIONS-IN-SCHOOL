81ea86e6b1740b5a9b739888385a940d00f3f44a
name <email>
2022-12-07 15:50:25 +0800

    yuanZhu

---
+++ string_PK.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int compare(char a[], char b[]);
+    char a[100];
+    char b[100];
+    int ret;
+    printf("input a:\n");
+    gets(a);
+    printf("input b:\n");
+    gets(b);
+    ret = compare(a[],b[]);
+}
+
+int compare(char a[], char b[]){
+
+}
