42ba810f28710f4a8355cd4ca6181e26c6c7eab7
SPGLP <1739255689@qq.com>
2023-05-04 16:30:10 +0800

    2022 5 4 WinTest 2

--- costWintest/WinComponent.java
+++ costWintest/WinComponent.java
@@ -2,9 +2,12 @@ package costWintest;
 import java.awt.*;
 import javax.swing.*;
 
+import costWintest.WinListener.Meun1L;
+
 public class WinComponent extends JFrame{
     JTextArea textBox;
     JButton startB;
+    Meun1L meun1l;
     JButton exitB;
     JButton nocostB;
     JButton allmoneyB;
@@ -17,6 +20,8 @@ public class WinComponent extends JFrame{
         setLayout(new FlowLayout());
         textBox = new JTextArea("不交税，就雷普");
         startB = new JButton("开始/继续计算");
+        meun1l = new Meun1L();
+        startB.addActionListener(meun1l);
         exitB = new JButton("退出系统");
         nocostB = new JButton("统计未交税人的数量");
         allmoneyB = new JButton("统计交税总金额");
